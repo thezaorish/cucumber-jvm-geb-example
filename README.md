80910bd72a64c044fd4268ab9801bbc0fb49a511
Victor Balanica <victorbalanica@gmail.com>
2012-11-04 14:07:31 +0200

    integrating cucumber-jvm, geb and groovy into the project + basic configuration for using Firefox
    the pom configuration is inspired from http://pastebin.com/XEmhuxkK

---
+++ README.md
@@ -0,0 +1,5 @@
+Cucumber-jvm-geb-example
+
+skeleton project for demonstrating the integration between Cucumber-jvm and Geb
+
+to run the project: mvn clean test package
---
+++ src/test/groovy/GebConfig.groovy
@@ -0,0 +1,7 @@
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.RemoteWebDriver
+
+driver = {	
+	RemoteWebDriver driver = new FirefoxDriver()
+	driver
+}
