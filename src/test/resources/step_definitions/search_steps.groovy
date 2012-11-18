68ebdb136dda05afcf3bdf978f694b6b34ea1943
Victor Balanica <victorbalanica@gmail.com>
2012-11-18 20:09:12 +0200

    added the simpler implementation, also (without page objects)

--- src/test/resources/step_definitions/search_steps.groovy
+++ src/test/resources/step_definitions/search_steps.groovy
@@ -5,18 +5,29 @@ this.metaClass.mixin(cucumber.runtime.groovy.EN)
 this.metaClass.mixin(geb.Browser)
 
 Given(~'I am on the Google search page') { ->
-	to GoogleHomePage
-	at GoogleHomePage
+	browser.go('http://www.google.com/')
+	
+	// to GoogleHomePage
+	// at GoogleHomePage
 }
 
-When(~'I search for "(.+)"') { searchTerm -> 
-	searchFor(searchTerm)
+When(~'I search for "(.+)"') { searchTerm ->
+	browser.$('input', name: 'q').value(searchTerm)
+	browser.$('button', name: 'btnG').click()
+	 
+	// searchFor(searchTerm)
 }
 
 Then(~'I should see the results page') { ->
-	at GoogleResultsPage
+	Thread.sleep(1000)
+	def title = browser.page.title
+	assert title.endsWith("Google Search") || title.endsWith("Căutare Google")
+	
+	// at GoogleResultsPage
 } 
 
-Then(~'The first link should be "(.+)"') { searchTerm -> 
-	assertResultIs(1, searchTerm)
+Then(~'The first link should be "(.+)"') { searchTerm ->
+	assert $('div#ires ol li')[0].find('a').text() == searchTerm
+	 
+	// assertResultIs(1, searchTerm)
 }
